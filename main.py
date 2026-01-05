93f4bbf24d52198fc6130b5814dab30fb16a9965
j03m4r <joeemarr12345@gmail.com>
2026-01-05 11:19:00 -0600

    Added CORS for all clients

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from endpoints import api_bp
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET")
 app.register_blueprint(api_bp)
+
+CORS(app)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
